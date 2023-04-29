5ed5d964e709aaa19eff9cd36d45f25333e6b594
Mateusz <mateusz.ostro@o2.pl>
2023-04-29 18:05:21 +0200

    Added validation to backend

--- src/main/java/tacos/web/DesignTacosController.java
+++ src/main/java/tacos/web/DesignTacosController.java
@@ -3,12 +3,14 @@ package tacos.web;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import tacos.Ingredient;
 import tacos.Taco;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -45,7 +47,11 @@ public class DesignTacosController {
     }
 
     @PostMapping
-    public String processDesign(Taco design) {
+    public String processDesign(@Valid Taco design, Errors errors) {
+        if(errors.hasErrors()) {
+            return "design";
+        }
+
         log.info("Przetwarzanie projektu taco: " + design.getIngredients());
         return "redirect:/orders/current";
     }
